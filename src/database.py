4a26a0abed925160db331f337444bf1c71545de1
lilnho <nathanho2048@gmail.com>
2023-10-30 23:28:02 -0700

    Added table attributes to db

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+import sqlalchemy
 from sqlalchemy import create_engine
 
 def database_connection_url():
@@ -8,3 +9,7 @@ def database_connection_url():
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+metadata_obj = sqlalchemy.MetaData()
+carts = sqlalchemy.Table("carts", metadata_obj, autoload_with=engine)
+ledgers = sqlalchemy.Table("ledgers", metadata_obj, autoload_with=engine)
+potions = sqlalchemy.Table("potions", metadata_obj, autoload_with=engine)
